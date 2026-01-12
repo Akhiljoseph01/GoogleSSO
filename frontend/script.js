b20e893d8ce3585683ece083aeb1aea0d92b3f88
Akhiljoseph <akhil123joseph@gmail.com>
2026-01-12 23:20:16 +0530

    script

--- frontend/script.js
+++ frontend/script.js
@@ -1,23 +1,28 @@
-function login() {
+window.onload = function () {
   google.accounts.id.initialize({
     client_id: "421779667146-2iphh2quvfivv7v1iqciqu3lrpv18ghu.apps.googleusercontent.com",
-    callback: handleGoogleResponse
+    callback: handleGoogleResponse,
+    auto_select: false,
+    cancel_on_tap_outside: true,
   });
 
-  google.accounts.id.prompt();
-}
+  google.accounts.id.renderButton(
+    document.getElementById("google-btn"),
+    { theme: "outline", size: "large" }
+  );
+};
 
 function handleGoogleResponse(response) {
   fetch("http://127.0.0.1:8000/auth/google/", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      token: response.credential
-    })
+      token: response.credential,
+    }),
   })
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
+    .then((res) => res.json())
+    .then((data) => console.log("SUCCESS:", data))
+    .catch((err) => console.error("ERROR:", err));
 }
