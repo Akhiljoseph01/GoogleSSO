783572dfca9c761b2ceea90b5677792574c4ec94
Akhiljoseph <akhil123joseph@gmail.com>
2026-01-12 22:51:32 +0530

    backend setup

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -38,7 +38,20 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'corsheaders',
 ]
+
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    ...
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,23 @@
 function login() {
   google.accounts.id.initialize({
     client_id: "421779667146-2iphh2quvfivv7v1iqciqu3lrpv18ghu.apps.googleusercontent.com",
-    callback: handleResponse
+    callback: handleGoogleResponse
   });
 
   google.accounts.id.prompt();
 }
 
-function handleResponse(response) {
+function handleGoogleResponse(response) {
   fetch("http://127.0.0.1:8000/auth/google/", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ token: response.credential })
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      token: response.credential
+    })
   })
   .then(res => res.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
 }
